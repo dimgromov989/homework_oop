81fe5e6dae0ffb6b7f35debe5c2a5d93bdb19e3f
Артем Громов <my_email.ru>
2025-12-06 18:26:39 +0300

    fixed_new

--- src/category.py
+++ src/category.py
@@ -1,4 +1,3 @@
-from src.products import Product
 
 
 class Category:
@@ -7,6 +6,7 @@ class Category:
     list_products: list
     quantity_categories = 0
     quantity_products = 0
+
     def __init__(self, name, description, list_products):
         self.name = name
         self.description = description
@@ -14,19 +14,13 @@ class Category:
         Category.quantity_categories += 1
         Category.quantity_products += len(list_products) if list_products else 0
 
-if __name__ == '__main__':
-    product_banana = Product(name="Банан", description="жёлтый", price=15.0, quantity=3)
-    product_pomidor = Product(name="Помидор", description="азербайджанский", price=3.0, quantity=10)
-    category_food = Category(name="Еда", description="Продукты питания", list_products=[product_banana, product_pomidor])
 
-    print(category_food.name)
-    print(category_food.list_products[0].name)
-    print(category_food.quantity_products)
-    print(category_food.quantity_categories)
-
-
-
-
-
-
-
+# if __name__ == '__main__':
+#     product_banana = Product(name="Банан", description="жёлтый", price=15.0, quantity=3)
+#     product_pomidor = Product(name="Помидор", description="азербайджанский", price=3.0, quantity=10)
+#     category_food = Category(name="Еда", description="Продукты питания", list_products=[product_banana, product_pomidor])
+#
+#     print(category_food.name)
+#     print(category_food.list_products[0].name)
+#     print(category_food.quantity_products)
+#     print(category_food.quantity_categories)
--- src/utils.py
+++ src/utils.py
@@ -7,7 +7,7 @@ from src.products import Product
 
 def read_json(path: str):
     file_path = Path(path)
-    with open(file_path, mode='r', encoding='utf-8') as file:
+    with open(file_path, mode="r", encoding="utf-8") as file:
         data = json.load(file)
         return data
 
@@ -19,18 +19,7 @@ def create_obj_from_json(data):
         cat_obj = Category(
             name=category["name"],
             description=category["description"],
-            list_products=products
+            list_products=products,
         )
         categories.append(cat_obj)
     return categories
-
-
-
-
-if __name__ == '__main__':
-    result = read_json("../data/products.json")
-    obj_result = create_obj_from_json(result)
-
-    print(obj_result)
-
-
